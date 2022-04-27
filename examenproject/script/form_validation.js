c0237fb4ccbfcaacc04e9d2f0e798d974527dcd4
JonasTheGamer <38383712+JonasTheGamer@users.noreply.github.com>
2022-04-27 11:09:13 +0200

    Fix form validation errors

--- examenproject/script/form_validation.js
+++ examenproject/script/form_validation.js
@@ -1,61 +1,61 @@
-// Constante variabelen
-const iptAchternaam = document.getElementById("iptAchternaam");
-const validationAchternaam = document.getElementById("validationAchternaam");
-
-const iptEmail = document.getElementById("iptEmail");
-const validationEmail = document.getElementById("validationEmail");
-
-const validationCompleteForm = document.getElementById("validationCompleteForm");
-
-// Handige functies
-function toonFout(htmlElement, foutmelding) {
-    htmlElement.style.display = "block";
-    htmlElement.innerHTML = foutmelding;
-}
-
-function verbergFout(htmlElement) {
-    htmlElement.style.display = "none";
-}
-
-// Validatie functies
-function valideerAchternaam() {
-    if (iptAchternaam.value.trim().length < 5) {
-        toonFout(validationAchternaam, "Je ingevoerde waarde is kleiner dan 5 tekens.");
-        return false;
-    } else {
-        verbergFout(validationAchternaam);
-        return true;
-    }
-}
-
-function valideerEmail() {
-    // Regex to match e-mail requirements
-    const regex = /^([a-z0-9]+\.)+[a-z0-9]+@(student.)?kdg.be$/gi;
-
-    // Check if the input matches the regex
-    if (!regex.test(iptEmail.value)) {
-        toonFout(validationEmail, "Je ingevoerde e-mail behoort niet tot het KdG domein.");
-        return false;
-    } else {
-        verbergFout(validationEmail);
-        return true;
-    }
-}
-
-function valideerFormulier(event) {
-
-    // Check of de waarden valide zijn (checkt automatisch ook of ze ingevuld zijn. Achternaam lengte is onder 5 indien leeg & email regex matcht niet indien leeg)
-    let achternaamOk = valideerAchternaam();
-    let emailOk = valideerEmail();
-    
-    if(!achternaamOk || !emailOk) {
-        event.preventDefault();
-        toonFout(validationCompleteForm, "Niet alle velden zijn correct ingevuld.")
-    }
-}
-
 // Bij het laden van de pagina
 window.addEventListener('DOMContentLoaded', () => {
+    // Constante variabelen
+    const iptAchternaam = document.getElementById("iptAchternaam");
+    const validationAchternaam = document.getElementById("validationAchternaam");
+
+    const iptEmail = document.getElementById("iptEmail");
+    const validationEmail = document.getElementById("validationEmail");
+
+    const validationCompleteForm = document.getElementById("validationCompleteForm");
+
+    // Handige functies
+    function toonFout(htmlElement, foutmelding) {
+        htmlElement.style.display = "block";
+        htmlElement.innerHTML = foutmelding;
+    }
+
+    function verbergFout(htmlElement) {
+        htmlElement.style.display = "none";
+    }
+
+    // Validatie functies
+    function valideerAchternaam() {
+        if (iptAchternaam.value.trim().length < 5) {
+            toonFout(validationAchternaam, "Je ingevoerde waarde is kleiner dan 5 tekens.");
+            return false;
+        } else {
+            verbergFout(validationAchternaam);
+            return true;
+        }
+    }
+
+    function valideerEmail() {
+        // Regex to match e-mail requirements
+        const regex = /^([a-z0-9]+\.)+[a-z0-9]+@(student.)?kdg.be$/gi;
+
+        // Check if the input matches the regex
+        if (!regex.test(iptEmail.value)) {
+            toonFout(validationEmail, "Je ingevoerde e-mail behoort niet tot het KdG domein.");
+            return false;
+        } else {
+            verbergFout(validationEmail);
+            return true;
+        }
+    }
+
+    function valideerFormulier(event) {
+
+        // Check of de waarden valide zijn (checkt automatisch ook of ze ingevuld zijn. Achternaam lengte is onder 5 indien leeg & email regex matcht niet indien leeg)
+        let achternaamOk = valideerAchternaam();
+        let emailOk = valideerEmail();
+
+        if (!achternaamOk || !emailOk) {
+            event.preventDefault();
+            toonFout(validationCompleteForm, "Niet alle velden zijn correct ingevuld.")
+        }
+    }
+
     // We kleuren het veld achternaam geel
     document.getElementById("iptAchternaam").style.backgroundColor = 'yellow';
 
@@ -69,7 +69,9 @@ window.addEventListener('DOMContentLoaded', () => {
     iptEmail.addEventListener('input', valideerEmail);
 
     // Form submit
-    document.getElementById("bestelFormulier").addEventListener("submit",valideerFormulier, false);
+    document.getElementById("bestelFormulier").addEventListener("submit", valideerFormulier, false);
+
+
 });
 
 
